2eac617e5680fff00dc3f1c0a21c95e16a96a7f6
shubhamwebdesign <hex.media.1800@gmail.com>
2023-02-24 10:00:04 +0530

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default {
+  displayName: 'fastify-sandbox',
+  preset: './jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/fastify-sandbox',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+};
